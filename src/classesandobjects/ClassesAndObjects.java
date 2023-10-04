120bfc83642c131fe33007c3b22334315c6e82d5
Sam Weiss <114678287+sweisscct@users.noreply.github.com>
2023-10-04 12:46:31 +0100

    Triangle Sides in Arrays
    Made a triangle class and 6 triangle objects. We tried to keep track of all the side lengths in arrays, but that got really confusing.
    
    To do: Encapsulate!

--- src/classesandobjects/ClassesAndObjects.java
+++ src/classesandobjects/ClassesAndObjects.java
@@ -14,7 +14,33 @@ public class ClassesAndObjects {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Triangle tri1 = new Triangle();
+        Triangle tri2 = new Triangle();
+        Triangle tri3 = new Triangle();
+        Triangle tri4 = new Triangle();
+        Triangle tri5 = new Triangle();
+        Triangle tri6 = new Triangle();
+
+        int[] sideLengths = new int[18];
+        // The lengths of tri1's sides will be first, then the lengths of tri2's sides etc
+        sideLengths[0] = 5;
+        sideLengths[1] = 8;
+        sideLengths[2] = 9;
+        
+        // tri5 (4, 7, 2)
+        sideLengths[12] = 4;
+        sideLengths[13] = 7;
+        sideLengths[14] = 2;
+        
+        // We want to set the middle side of each triangle to be 3
+        sideLengths[1] = 3;
+        sideLengths[4] = 3;
+        sideLengths[7] = 3;
+        sideLengths[10] = 3;
+        sideLengths[13] = 3;
+        sideLengths[16] = 3;
+        
+        
     }
     
 }
---
+++ src/classesandobjects/Triangle.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classesandobjects;
+
+/**
+ *
+ * @author Lecturer
+ */
+public class Triangle {
+    int side1;
+    int side2;
+    int side3;
+}
