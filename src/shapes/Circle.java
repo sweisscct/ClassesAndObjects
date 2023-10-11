23300a8a274a76bfa8f25bacbc9814fe8e4a296a
Sam Weiss <114678287+sweisscct@users.noreply.github.com>
2023-10-11 13:16:10 +0100

    Made Circle and Square classes

---
+++ src/shapes/Circle.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package shapes;
+
+/**
+ *
+ * @author Lecturer
+ */
+public class Circle extends Shape {
+    private double radius;
+    
+    @Override
+    public double getArea() {
+        return 3.14 * radius * radius;
+    }
+}
